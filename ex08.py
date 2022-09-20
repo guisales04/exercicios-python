d15acad4d887242eb812c2e1ae8cd52c741855a3
GuilhermeSales04 <guilhermeterra026@gmail.com>
2022-09-19 21:01:39 -0300

    FizzBuzz

--- ex08.py
+++ ex08.py
@@ -1,5 +1,5 @@
 """
-    Fizz Buzz - Se o parâmetro da função for divisível por 2,
+    Fizz Buzz - Se o parâmetro da função for divisível por 3,
     retorne Fizz. Se o parâmetro da função for divisível por 5,
     retorne Buzz. Se o parâmetro da função for divisível por 5 e 3,
     retorne FizzBuzz, caso contrário, retorne o número enviado.
@@ -7,14 +7,14 @@
 
 
 def fizz_buzz(num: int = 0) -> str | int:
-    if num % 2 == 0:
-        return 'Fizz'
-    elif num % 5 == 0 and num % 3 == 0:
+    if num % 5 == 0 and num % 3 == 0:
         return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
     elif num % 5 == 0:
         return 'Buzz'
     else:
         return num
 
 
-print(fizz_buzz(int(input('Insira um número: '))))
+print(fizz_buzz(int(input('Digite um número: '))))
